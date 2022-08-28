d015d998b3f8b9181c2c7c5497c68c896ac011a5
Leonard <leonard@mimisiku.dev>
2022-08-28 15:20:55 +0200

    fix test

--- src/api.spec.ts
+++ src/api.spec.ts
@@ -19,11 +19,6 @@ describe('Api', () => {
         username: faker.internet.email(),
         password: faker.internet.password(),
       };
-      const data = new URLSearchParams();
-      data.set('grant_type', 'password');
-      data.set('client_id', 'mpp-app');
-      data.set('username', creds.username);
-      data.set('password', creds.password);
 
       const mockedResponse: Partial<z.infer<typeof AuthenticationTokenOutput> | RemoteError> = {
         token: faker.datatype.string(100),
@@ -31,7 +26,7 @@ describe('Api', () => {
 
       mockPool
         .intercept({
-          path: 'auth/realms/mpp-prod/protocol/openid-connect/token',
+          path: 'customers/email/sign-in',
           method: 'POST',
         })
         .reply(200, mockedResponse);
